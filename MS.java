8a600ff266312032f0560e6356985a23abf6e015
Esaw Adhana <88061109+EsawAdhana@users.noreply.github.com>
2023-08-20 15:48:45 -0700

    Add files via upload

---
+++ MS.java
@@ -0,0 +1,38 @@
+package MS155;
+
+import java.util.Stack;
+
+class MS {
+    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> cache = new Stack<>();
+    public MS() {
+
+    }
+    public void push(int val) {
+        stack.push(val);
+        if (cache.isEmpty()) {
+            cache.push(val);
+            return;
+        }
+        int min = cache.peek();
+        if (min > val) {
+            cache.push(val);
+        } else {
+            cache.push(min);
+        }
+    }
+
+    public void pop() {
+        cache.pop();
+        stack.pop();
+    }
+
+    public int top() {
+        cache.peek();
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return cache.peek();
+    }
+}
