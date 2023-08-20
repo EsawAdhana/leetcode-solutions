b9004831de82fc1f24bec13781ae19832e08fdd7
Esaw Adhana <88061109+EsawAdhana@users.noreply.github.com>
2023-08-20 15:23:05 -0700

    Add files via upload

---
+++ BSC.java
@@ -0,0 +1,24 @@
+package BSC844;
+
+import java.util.Stack;
+
+class BSC {
+    public boolean backspaceCompare(String s, String t) {
+        return convert(s).equals(convert(t));
+    }
+    private static Stack<Character> convert(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (!(str.charAt(i) == '#')) {
+                stack.push(str.charAt(i));
+            } else {
+                try {
+                    stack.pop();
+                } catch (Exception e) {
+
+                }
+            }
+        }
+        return stack;
+    }
+}
