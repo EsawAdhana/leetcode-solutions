ade5349f854eec3501fc3490395b6ac2eb82d438
Esaw Adhana <88061109+EsawAdhana@users.noreply.github.com>
2023-08-21 14:13:05 -0700

    Add files via upload

---
+++ VP20.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+class VP20 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char curr = s.charAt(i);
+            if (curr == '(' || curr == '[' || curr == '{') {
+                stack.push(curr);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                if (curr == ')') {
+                    if (stack.pop() != '(') {
+                        return false;
+                    }
+                } else if (curr == ']') {
+                    if (stack.pop() != '[') {
+                        return false;
+                    }
+                } else {
+                    if (stack.pop() != '{') {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
