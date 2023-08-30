8702ac3ef48c20535d05b9a3cf86a4b4ad07daca
Esaw Adhana <88061109+EsawAdhana@users.noreply.github.com>
2023-08-30 12:05:24 -0700

    Add files via upload

---
+++ ST100.java
@@ -0,0 +1,21 @@
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+
+class Solution {
+    ArrayList<Integer> pArr = new ArrayList<>();
+    ArrayList<Integer> qArr = new ArrayList<>();
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        dfs(p, pArr);
+        dfs(q, qArr);
+        return pArr.equals(qArr);
+    }
+    public void dfs(TreeNode node, ArrayList<Integer> list) {
+        if (node == null) {
+            list.add(Integer.MAX_VALUE);
+            return;
+        }
+        list.add(node.val);
+        dfs(node.left, list);
+        dfs(node.right, list);
+    }
+}
