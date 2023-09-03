d2442c8dbc45e6db7ff827b7b8c41302a31cb2cc
Esaw Adhana <88061109+EsawAdhana@users.noreply.github.com>
2023-09-03 15:34:12 -0700

    Add files via upload

---
+++ VP125.java
@@ -0,0 +1,22 @@
+class VP125 {
+    public boolean isPalindrome(String s) {
+        if (s.length() < 2) return true;
+        s = s.toLowerCase();
+        int p1 = 0;
+        int p2 = s.length() - 1;
+        while (p1 < p2) {
+            if (!Character.isLetterOrDigit(s.charAt(p1))) {
+                p1++;
+            } else if (!Character.isLetterOrDigit(s.charAt(p2))) {
+                p2--;
+            } else {
+                if (s.charAt(p1) != s.charAt(p2)) {
+                    return false;
+                }
+                p1++;
+                p2--;
+            }
+        }
+        return true;
+    }
+}
