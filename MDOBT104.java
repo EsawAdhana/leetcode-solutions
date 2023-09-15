f15ebba52d5a24649ff3ad40608159a07cef4f9a
Esaw Adhana <88061109+EsawAdhana@users.noreply.github.com>
2023-09-14 22:34:11 -0700

    Add files via upload

---
+++ MDOBT104.java
@@ -0,0 +1,10 @@
+import javax.swing.tree.TreeNode;
+
+class MDOBT104 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+}
