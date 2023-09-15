07a5625955d46f6f81129d51a54f14bff41523cf
Esaw Adhana <88061109+EsawAdhana@users.noreply.github.com>
2023-09-14 22:19:21 -0700

    Update VA242.java

--- VA242.java
+++ VA242.java
@@ -1,14 +1,23 @@
 // Question #242: Valid Anagram
-// Time complexity: O(n*log(n)) && Space complexity: O(s + t)
+// Time complexity: O(n) && Space complexity: O(s + t)
 
 import java.util.Arrays;
 
 class VA242 {
-    public boolean isAnagram(String s, String t) {
-        char[] sArr = s.toCharArray();
-        char[] tArr = t.toCharArray();
-        Arrays.sort(sArr);
-        Arrays.sort(tArr);
-        return Arrays.equals(sArr, tArr);
+   public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (count[s.charAt(i) - 'a'] != 0) return false;
+        }
+
+        return true;
     }
 }
