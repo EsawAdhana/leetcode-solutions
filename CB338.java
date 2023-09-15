72e8d3540a1c55d4464b656fa07c89604cb7f711
Esaw Adhana <88061109+EsawAdhana@users.noreply.github.com>
2023-09-14 21:48:57 -0700

    Add files via upload

---
+++ CB338.java
@@ -0,0 +1,7 @@
+class CB338 {
+    public int[] countBits(int n) {
+        int[] arr = new int[n + 1];
+        for (int i = 0; i < arr.length; i++) arr[i] = arr[i / 2] + i % 2;
+        return arr;
+    }
+}
