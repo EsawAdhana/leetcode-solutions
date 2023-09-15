ef38294468acee7092e0a01262f2cded307ade3c
Esaw Adhana <88061109+EsawAdhana@users.noreply.github.com>
2023-09-14 21:51:40 -0700

    Update MN268.java

--- MN268.java
+++ MN268.java
@@ -1,3 +1,6 @@
+// Question #268: Missing Number
+// Time complexity: O(n) && Space complexity: O(1)
+
 class MN268 {
     public int missingNumber(int[] nums) {
         int num = (nums.length * (nums.length + 1)) / 2;
