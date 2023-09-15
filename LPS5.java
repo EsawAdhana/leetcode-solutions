38bd815468ddeca242a502d4270918a2e395bab3
Esaw Adhana <88061109+EsawAdhana@users.noreply.github.com>
2023-09-14 22:25:37 -0700

    Add files via upload

---
+++ LPS5.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+class LPS5 {
+    int start = 0;
+    int maxLength = 0;
+    public String longestPalindrome(String s) {
+        if (s.length() <= 1) return s;
+
+        for (int i = 0; i < s.length(); i++) {
+            findPalindrome(s, i, i);
+            findPalindrome(s, i, i + 1);
+        }
+
+        return s.substring(start, start + maxLength);
+    }
+
+    private void findPalindrome(String s, int i, int j) {
+        while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
+            i--;
+            j++;
+        }
+
+        if (maxLength < j - i - 1) {
+            maxLength = j - i - 1;
+            start = i + 1;
+        }
+    }
+}
