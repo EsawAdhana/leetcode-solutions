26dadda35048307fd9ed35b6478c226f94d8491c
Esaw Adhana <88061109+EsawAdhana@users.noreply.github.com>
2023-09-14 22:13:11 -0700

    Add files via upload

---
+++ RL143.java
@@ -0,0 +1,41 @@
+class RL143 {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) return;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode mid = slow.next;
+        slow.next = null;
+
+        mid = reverse(mid);
+
+        while(mid != null) {
+            ListNode tmp1 = head.next;
+            ListNode tmp2 = mid.next;
+
+            head.next = mid;
+            mid.next = tmp1;
+
+            head = tmp1;
+            mid = tmp2;
+        }
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
