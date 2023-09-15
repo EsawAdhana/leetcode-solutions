42382b5245b425a01be7a2e0d50918aa849abfad
Esaw Adhana <88061109+EsawAdhana@users.noreply.github.com>
2023-09-14 21:46:15 -0700

    Update SOTI371.java

--- SOTI371.java
+++ SOTI371.java
@@ -1,3 +1,6 @@
+// Question #371: Sum of Two Integers
+// Time complexity: O(1) && Space complexity: O(1)
+
 class SOTI371 {
     public int getSum(int a, int b) {
         return b == 0 ? a : getSum(a ^ b, (a & b) << 1);
