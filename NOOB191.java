f99f44f8d441994841c78ecea66414984a6eeec8
Esaw Adhana <88061109+EsawAdhana@users.noreply.github.com>
2023-09-14 21:47:12 -0700

    Add files via upload

---
+++ NOOB191.java
@@ -0,0 +1,10 @@
+class NOOB191 {
+    public int hammingWeight(int n) {
+        int ones = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n & 1) == 1) ++ones;
+            n >>= 1;
+        }
+        return ones;
+    }
+}
