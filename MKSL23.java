25d3b0172a11d3d1bc80a9347bad12d46df59e64
Esaw Adhana <88061109+EsawAdhana@users.noreply.github.com>
2023-09-14 22:09:48 -0700

    Add files via upload

---
+++ MKSL23.java
@@ -0,0 +1,27 @@
+class MKSL23 {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) return null;
+        return mergeSort(lists, 0, lists.length - 1);
+    }
+
+    public ListNode mergeSort(ListNode[] lists, int start, int end) {
+        if (start == end) return lists[start];
+        int mid = start + (end - start) / 2;
+        ListNode left = mergeSort(lists, start, mid);
+        ListNode right = mergeSort(lists, ++mid, end);
+        return mergeTwoLists(left, right);
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        if (list1.val >= list2.val) {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        } else {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        }
+    }
+}
