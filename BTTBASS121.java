f986c3584eeb963b0c3fa98ebb80a4390d2cdeea
Esaw Adhana <88061109+EsawAdhana@users.noreply.github.com>
2023-09-14 21:21:49 -0700

    Add files via upload

---
+++ BTTBASS121.java
@@ -0,0 +1,13 @@
+class BTTBASS121 {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int min = prices[0];
+
+        for (int i = 0; i < prices.length; i++) {
+            maxProfit = Math.max(prices[i] - min, maxProfit);
+            min = Math.min(min, prices[i]);
+        }
+
+        return maxProfit;
+    }
+}
